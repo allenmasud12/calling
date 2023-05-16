2453b281a1b92b2c7eff595f6119521c12ec703a
Allen Masud <allenmasud12@gmail.com>
2023-05-16 14:53:06 +0600

    Initial commit

---
+++ lib/utils/settings.dart
@@ -0,0 +1,4 @@
+const appId = '9642e775b6604397b30ce374a77f1575';
+const channgeId = "firstchannel";
+const token =
+    '007eJxTYPjk7ZE7aWuL96xepRfn1E0W/FM8+LKX3eaq4xxRY0+7dw4KDJZmJkap5uamSWZmBibGluZJxgbJqcbmJonm5mmGpuamBfzJKQ2BjAw6634xMEIhiM/DkJZZVFySnJGYl5eaw8AAAAwqIT4=';
